96f560fdf6a522735e07a15fb902d0a234a9886d
Aldovani <aldovane.etec@gmail.com>
2021-04-30 21:11:04 -0300

    atualização total

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 const div = document.querySelector(".container");
 const button = document.querySelector("button");
-const input = document.querySelector("input[type='text']");
+let contI = 1;
+let n = 24;
 
-button.addEventListener("click", async (e) => {
+document.querySelector("script").addEventListener("load",pokemon);
+button.addEventListener("click", (e) => {
   e.preventDefault();
-  if (input.value == "") return;
-
-  const pokemon = await getPokemon(input.value);
-
-  console.log(pokemon);
-  createCard(pokemon);
-  input.value = "";
+  pokemon();
 });
-
+async function pokemon() {
+  const pokemon = await getPokemon(n);
+  pokemon.map((e) => {
+    createCard(e);
+  });
+}
 async function getPokemon(txt) {
-  console.log(txt);
-  const pokemon = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${txt.toLocaleLowerCase()}`
-  )
-    .then((response) => response.json())
-    .then((pokemon) => pokemon)
-    .catch((err) => {
-      console.log(err);
-      openModal();
-    });
-  return pokemon;
+  let pokemonList = [];
+  for (contI; contI <= Number(txt); contI++) {
+    console.log(contI);
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${contI}`)
+      .then((response) => response.json())
+      .then((pokemon) => pokemon)
+      .catch((err) => {
+        console.log(err);
+       
+      });
+      pokemonList.push(pokemon);
+  }
+  n += 12;
+  return pokemonList;
 }
 
 function createCard(pokemon) {
   let listStatsElement = "";
   let listTypesElement = "";
-  let idConvertido;
+  let idConvertido = "";
 
   const { id, types, stats } = pokemon;
 
@@ -63,7 +67,7 @@ function createCard(pokemon) {
     <img src="${pokemon.sprites.front_default}"/>
     <section class="info section">
       <span class="idPokemon" >#${idConvertido}</span>
-      <h3>${pokemon.name}</h3>
+      <h3 class="namePokemon" >${pokemon.name}</h3>
       <ol>
         ${listTypesElement}  
       </ol>
@@ -73,43 +77,7 @@ function createCard(pokemon) {
         ${listStatsElement}
       </ol>
     </section>
-  </div>
-  
-  
-  `;
+  </div>`;
 
   div.innerHTML += card;
 }
-
-function openModal() {
-  console.log("Modal");
-  const modal = document.querySelector(".modal");
-
-  modal.setAttribute("class", "modal modal-active");
-
-  modal.querySelector("button").addEventListener("click", () =>modal.setAttribute("class", "modal"));
-}
-
-{
-  /* <section class="stats">
-<ol>
-  ${listStatsElement}
-</ol>
-</section> */
-}
-
-// function createPokedex() {
-
-//   pokemons.map((e) => {
-//     const cardHtml = `
-//     <li>
-//     <h3>${e.name}</h3>
-//     <img src="${e.sprites.front_default}" alt="sprit ${e.name}"/>
-
-//     </li>
-//     `;
-
-//     return div.querySelector('ol').innerHTML+=cardHtml
-//   })
-
-// }
