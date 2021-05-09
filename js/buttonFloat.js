71a2bcc702870b1a2770d330bc3b74419cefbca9
Aldovani <aldovane.etec@gmail.com>
2021-05-09 19:50:10 -0300

    update style button arrow

--- js/buttonFloat.js
+++ js/buttonFloat.js
@@ -1,20 +1,16 @@
 const buttons = document.querySelector('.button');
 
-window.addEventListener('scroll', (e) => {
+window.addEventListener('scroll', () => {
  const top =document.querySelector('header').getBoundingClientRect().top
-
-  if (-600 >= top) {
+console.log(top)
+  if (top <= -1000) {
     buttons.classList.add('visible')
   }
   else {
-    
     buttons.classList.remove('visible')
-  
   }
 
 })
-
-
 
 buttons.addEventListener('click', () => {
   scrollTo(0,0)
